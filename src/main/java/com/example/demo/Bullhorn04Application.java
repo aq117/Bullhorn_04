04dce1180da934a9b655d629379d8465de348112
RVSW317008DT <RVSW317008DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-25 11:33:45 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Bullhorn04Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bullhorn04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bullhorn04Application.class, args);
+	}
+}
